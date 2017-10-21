32815c5eacf50dbdad889ed159cf510fa9b78731
DESKTOP-IOJV8SG\RudiAnton <anttonms@gmail.com>
2017-10-21 09:00:21 +0700

    no message

--- pendaftaranRinci/pendaftaranRinciModel.js
+++
@@ -1,7 +0,0 @@
-let mong = require('mongoose');
-let pendaftaranrinci = mong.Schema({
-	KdPendaftaran: String,
-    KdAplikasi : String,
-    Statua : String
-});
-let PendaftaranRinci = module.exports = mong.model("PendaftaranRinci", pendaftaranrinci, "PendaftaranRinci");
