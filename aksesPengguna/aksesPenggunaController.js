a4aebc92b0e07e8ba315b31802245bcbc86c507c
DESKTOP-IOJV8SG\RudiAnton <anttonms@gmail.com>
2017-10-21 09:08:49 +0700

    no message

--- aksesPengguna/aksesPenggunaController.js
+++ aksesPengguna/aksesPenggunaController.js
@@ -2,14 +2,14 @@ AksesPengguna=require('./aksesPenggunaModel.js');
 module.exports.getAksesPengguna=function(callback,limit){
     AksesPengguna.find(callback).limit(limit);
 }
-module.exports.createAksesPengguna=function(aksespengguna,callback){
-    AksesPengguna.create(aksespengguna,callback);
+module.exports.createAksesPengguna=function(akses,callback){
+    AksesPengguna.create(akses,callback);
 }
 module.exports.removeAksesPengguna=function(_id,callback){
     AksesPengguna.findByIdAndRemove(_id,callback);
 }
-module.exports.updateAksesPengguna=function(_id,aksespengguna,callback){
-    AksesPengguna.findByIdAndUpdate(_id,aksespengguna,callback);
+module.exports.updateAksesPengguna=function(_id,akses,callback){
+    AksesPengguna.findByIdAndUpdate(_id,akses,callback);
 }
 module.exports.getAksesPenggunaById=function(id,callback){
     AksesPengguna.findById(id,callback);
--- aksesPengguna/aksesPenggunaRoute.js
+++ aksesPengguna/aksesPenggunaRoute.js
@@ -2,7 +2,7 @@ let expres = require('express');
 let route = expres.Router();
 let AksesPenggunaController=require('./aksesPenggunaController.js');
 
-route.get('/aksesPengguna', function (req, res) {
+route.get('/aksespengguna', function (req, res) {
     AksesPenggunaController.getAksesPengguna(function (error, respon) {
         if (error) {
             throw error;
@@ -10,16 +10,16 @@ route.get('/aksesPengguna', function (req, res) {
         res.json(respon);
     });
 });
-route.post('/aksesPengguna', function (req, res) {
-    let aksespengguna = req.body;
-    AksesPenggunaController.createAksesPengguna(aksespengguna,function (error, respon) {
+route.post('/aksespengguna', function (req, res) {
+    let akses = req.body;
+    AksesPenggunaController.createAksesPengguna(akses,function (error, respon) {
         if (error) {
             throw error;
         }
         res.json(respon);
     });
 });
-route.delete('/aksesPengguna/:_id', function (req, res) {
+route.delete('/aksespengguna/:_id', function (req, res) {
     AksesPenggunaController.removeAksesPengguna(req.params._id,function (error, respon) {
         if (error) {
             throw error;
@@ -27,16 +27,16 @@ route.delete('/aksesPengguna/:_id', function (req, res) {
         res.json(respon);
     });
 });
- route.put('/aksesPengguna/:_id', function (req, res) {
-     let aksespengguna = req.body;
-     AksesPenggunaController.updateAksesPengguna(req.params._id,aksespengguna,function (error, respon) {
+ route.put('/aksespengguna/:_id', function (req, res) {
+     let akses = req.body;
+     AksesPenggunaController.updateAksesPengguna(req.params._id,akses,function (error, respon) {
          if (error) {
              throw error;
          }
          res.json(respon);
      });
  });
- route.get('/aksesPengguna/:_id', function (req, res) {
+ route.get('/aksespengguna/:_id', function (req, res) {
      let id = req.params._id;
      AksesPenggunaController.getAksesPenggunaById(id, function (error, respon) {
         if (error) {
